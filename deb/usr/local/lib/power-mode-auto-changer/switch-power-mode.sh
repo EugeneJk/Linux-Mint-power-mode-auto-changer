f42e47d37f0a4e65749747d99d3599deffbbfbf9
Лазарчук Евгений <shadyjk@yandex.ru>
2026-02-14 12:14:31 -0500

    Added worked script with notyfication about power mode change

--- deb/usr/local/lib/power-mode-auto-changer/switch-power-mode.sh
+++ deb/usr/local/lib/power-mode-auto-changer/switch-power-mode.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
+set -e
 
-PATH=/usr/sbin:/usr/bin:/sbin:/bin
+# Определяем активного пользователя (текущий сеанс)
+ACTIVE_USER=$(loginctl list-sessions --no-legend | awk '{print $3}' | head -n1)
+USER_CONFIG="/home/$ACTIVE_USER/.config/power-mode-auto-changer/power-modes.conf"
 
-LOG_DIR="/var/tmp/power-mode-auto-changer"
-LOG_FILE="$LOG_DIR/test.log"
+if [ ! -f "$USER_CONFIG" ]; then
+    echo "Config not found: $USER_CONFIG"
+    exit 1
+fi
+source "$USER_CONFIG"
 
-mkdir -p "$LOG_DIR"
+STATE=$(cat "$AC_PATH")
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') - service triggered" >> "$LOG_FILE"
+if [ "$STATE" = "1" ]; then
+    POWER_MODE="$ON_AC"
+    NOTIFY_ICON="ac-adapter-symbolic"
+else
+    POWER_MODE="$ON_BATTERY"
+    NOTIFY_ICON="battery-symbolic"
+fi
 
-exit 0
+case "$POWER_MODE" in
+    "power-saver")
+        NOTIFY_TEXT="Activated mode: Power saver"
+        ;;
+    "balanced")
+        NOTIFY_TEXT="Activated mode: Balanced"
+        ;;
+    "performance")
+        NOTIFY_TEXT="Activated mode: Performance"
+        ;;
+esac
+
+powerprofilesctl set "$POWER_MODE"
+
+ACTIVE_UID=$(id -u "$ACTIVE_USER")
+
+DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$ACTIVE_UID/bus"
+
+sudo -u "$ACTIVE_USER" \
+DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
+gdbus call --session \
+  --dest org.Cinnamon \
+  --object-path /org/Cinnamon \
+  --method org.Cinnamon.ShowOSD \
+  "{'icon': <'$NOTIFY_ICON'>, 'label': <'$NOTIFY_TEXT'>}"
