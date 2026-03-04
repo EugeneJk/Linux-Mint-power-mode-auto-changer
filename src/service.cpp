f49b8ee6d12b73c41944110a3a0c67d22cbafb9a
Лазарчук Евгений <shadyjk@yandex.ru>
2026-03-03 20:04:19 -0500

    Added logging for service

--- src/service.cpp
+++ src/service.cpp
@@ -15,23 +15,27 @@
 #include <unistd.h>
 #include <cstring>
 #include <cerrno>
+#include <syslog.h>
+
 int main()
 {
+    openlog(APP_NAME.c_str(), LOG_PID, LOG_DAEMON);
+    syslog(LOG_INFO, "Started ...");
     // Create blocking to avoid run the same simultaniously
     int fd = open(LOCK_FILE, O_CREAT | O_RDWR, 0644);
     if (fd == -1)
     {
-        std::cerr << "open() failed: "
-                  << std::strerror(errno) << std::endl;
+        syslog(LOG_ERR, "open() failed: %s", std::strerror(errno));
+        closelog();
         return 1;
     }
 
     // wait for lock release if execution in the enother process
     if (flock(fd, LOCK_EX) == -1)
     {
-        std::cerr << "flock() failed: "
-                  << std::strerror(errno) << std::endl;
+        syslog(LOG_ERR, "flock() failed: %s", std::strerror(errno));
         close(fd);
+        closelog();
         return 1;
     }
 
@@ -58,12 +62,14 @@ int main()
         std::string icon;
         if (isAcOn)
         {
+            syslog(LOG_INFO, "Power cable is plugged in");
             displayText = userConfig.onAcText;
             newPowerProfile = powerProfileToString(userConfig.onAc);
             icon = isCinnamon ? ON_AC_ICON : ON_AC_ICON_OTHER;
         }
         else
         {
+            syslog(LOG_INFO, "Power cable is unplugged");
             displayText = userConfig.onBatText;
             newPowerProfile = powerProfileToString(userConfig.onBat);
             icon = isCinnamon ? ON_BAT_ICON : ON_BAT_ICON_OTHER;
@@ -75,29 +81,39 @@ int main()
         // if new profile is the same as current then do nothing and finish
         if (currentPowerProfile == newPowerProfile)
         {
+            syslog(LOG_INFO, "Current profile is the same as required (%s) - no action needed.", currentPowerProfile.c_str());
             // release lock
             flock(fd, LOCK_UN);
             close(fd);
+            closelog();
             return 0;
         }
 
+        syslog(LOG_INFO, "Switching from  %s to %s", currentPowerProfile.c_str(), newPowerProfile.c_str());
         // set new profile
         execCommand("powerprofilesctl set " + newPowerProfile);
 
+        std::string updatedPowerProfile = execCommand("powerprofilesctl get");
+        syslog(LOG_INFO, "Profile after switch: %s", updatedPowerProfile.c_str());
+
         // show notification
+        syslog(LOG_INFO, "Showing %s noticiation", isCinnamon ? "OSD" : "Default");
         showNotification(user, icon, displayText, isCinnamon);
+        syslog(LOG_INFO, "Finished");
     }
     catch (const std::exception &ex)
     {
-        std::cerr << "Error: " << ex.what() << "\n";
+        syslog(LOG_ERR, "Error: %s" , ex.what());
         // silent exit with error
         // release lock
         flock(fd, LOCK_UN);
         close(fd);
+        closelog();
         return 1;
     }
     // release lock
     flock(fd, LOCK_UN);
     close(fd);
+    closelog();
     return 0;
 }
